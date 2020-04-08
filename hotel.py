58f7d8b8c633473a2b8dbb3e29c904bcb13d4f86
Chris Aquino <chris@radishmouse.com>
2020-04-08 05:41:11 -0400

    adds is_vacant

--- hotel.py
+++ hotel.py
@@ -1,36 +1,41 @@
 #hotel app
 
+EMPTY_ROOM = {}
+
 hotel = {
-    '101': '',
-    '102': '',
-    '103': '',
-    '104': '',
-    '105': '',
+    '101': {
+        'guest': {
+            'name': 'Elliot Alderson',
+            'phone': 8675309,
+	   'prepaid': True,
+        }
+    },
+    '102': {},
+    '103': {},
+    '104': {
+        'guest': {
+            'name': 'Darlene Alderson',
+            'phone': 4567890,
+	   'prepaid': False
+        }
+    },
+    '105': {},
 }
 
-elliot = {
-    'occupant_name': 'Elliot',
-    'phone_number': 8675309,
-    'has_prepaid': True
-}
-# guest is "checking in"
-hotel['101'] = elliot
-
-# customer = {}
-# name = input('What is the name of the guest? ')
-# customer['name'] = name
-
-# hotel['104'] = customer
-
+def is_vacant(which_hotel, which_room):
+	if which_hotel[which_room] == EMPTY_ROOM:
+		return True
+	else:
+		return False
 
 # Printing the room status
 for room_number in hotel.keys():
     # The room_number variable will be a 
     # string, like '101' or '103'
-    if hotel[room_number] == '':
+    if is_vacant(hotel, room_number):
         print(f'{room_number} is vacant')
     else:
-        print(f'{room_number} is occupied by {hotel[room_number]["occupant_name"]}')
+        print(f'{room_number} is occupied by {hotel[room_number]["guest"]["name"]}')
 
 # For any one room, you should store:
 # - occupant name
